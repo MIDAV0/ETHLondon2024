227d60c947c11eee8f5181ab8a85cf955ae1344d
MIDAV0 <magilnyy.vadimo@gmail.com>
2024-03-16 08:29:21 +0000

    Updated packages

---
+++ packages/nextjs/app/freelancers/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Freelancers: NextPage = () => {
+  return (
+    <>
+      <div className="text-center mt-8 bg-secondary p-10">
+        <h1 className="text-4xl my-0">Freelancers</h1>
+      </div>
+    </>
+  );
+};
+
+export default Freelancers;
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -19,11 +19,11 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Home",
     href: "/",
   },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
+  // {
+  //   label: "Debug Contracts",
+  //   href: "/debug",
+  //   icon: <BugAntIcon className="h-4 w-4" />,
+  // },
 ];
 
 export const HeaderMenuLinks = () => {
@@ -89,12 +89,8 @@ export const Header = () => {
           )}
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+          <div className="flex">
+            <span className="font-extrabold">FREELANCE PLATFORM</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
@@ -103,7 +99,6 @@ export const Header = () => {
       </div>
       <div className="navbar-end flex-grow mr-4">
         <RainbowKitCustomConnectButton />
-        <FaucetButton />
       </div>
     </div>
   );
